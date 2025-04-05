9831e0ffe8fb16c91adbadd4a16d1945fd53e0d7
Ivan "ukrustacean" Dereviankin <ukrustacean@gmail.com>
2025-04-05 15:06:46 +0200

    Implement basic solving logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", solve(2., 1., -3.));
+}
+
+fn solve(a: f64, b: f64, c: f64) -> [f64; 2] {
+    let d = (b.powi(2) - 4.0 * a * c).sqrt();
+    [
+        (-b + d) / 2.0 / a,
+        (-b - d) / 2.0 / a,
+    ]
 }
